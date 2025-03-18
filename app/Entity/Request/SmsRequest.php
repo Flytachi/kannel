42d6b537eb3b4e68efa67279e0d6b1d803399b86
flytachi <jasur.rakhmatov03@gmail.com>
2025-03-18 14:47:43 +0500

    fix bags

--- app/Entity/Request/SmsRequest.php
+++ app/Entity/Request/SmsRequest.php
@@ -2,13 +2,20 @@
 
 namespace App\Entity\Request;
 
+use Flytachi\Kernel\Src\Errors\ClientError;
 use Flytachi\Kernel\Src\Stereotype\Request;
 
 class SmsRequest extends Request
 {
     public function __construct(
-        public int $phoneNumber,
+        public int|string $phoneNumber,
         public string $message
     ) {
+        if (is_string($this->phoneNumber)) {
+            if (!is_numeric($this->phoneNumber)) {
+                ClientError::throw("Phone number must be a numeric value");
+            }
+            $this->phoneNumber = (int) $this->phoneNumber;
+        }
     }
 }
--- app/Entity/Request/SubmitRequest.php
+++ app/Entity/Request/SubmitRequest.php
@@ -2,14 +2,24 @@
 
 namespace App\Entity\Request;
 
+use Flytachi\Kernel\Src\Errors\ClientError;
 use Flytachi\Kernel\Src\Stereotype\Request;
 
 class SubmitRequest extends Request
 {
     public function __construct(
-        public int $phoneNumber,
+        public int|string $phoneNumber,
         public string $message,
-        public bool $enableInput = false,
+        public bool|string $enableInput = false,
     ) {
+        if (is_string($this->phoneNumber)) {
+            if (!is_numeric($this->phoneNumber)) {
+                ClientError::throw("Phone number must be a numeric value");
+            }
+            $this->phoneNumber = (int) $this->phoneNumber;
+        }
+        if (is_string($this->enableInput)) {
+            $this->enableInput = (bool) $this->enableInput;
+        }
     }
 }
